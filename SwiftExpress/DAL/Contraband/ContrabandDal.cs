d298e82d684ef3874b97f3272641c9980add6fce
17520369303 <62336865+17520369303@users.noreply.github.com>
2020-03-20 11:57:45 +0800

    违禁品查询及运单查询

---
+++ SwiftExpress/DAL/Contraband/ContrabandDal.cs
@@ -0,0 +1,36 @@
+﻿using MODEL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL
+{
+    public class ContrabandDal
+    {
+        /// <summary>
+        /// 查询物品是否是违禁物品
+        /// </summary>
+        /// <param name="name"></param>
+        /// <param name="dh"></param>
+        /// <returns></returns>
+        public List<Contraband> GetContrabands(string name)
+        {
+            string sql = "select * from Contraband";
+            return DBHelper.GetToList<Contraband>(sql);
+        }
+
+        /// <summary>
+        /// 查询运单号查看信息
+        /// </summary>
+        /// <param name="name"></param>
+        /// <param name="dh"></param>
+        /// <returns></returns>
+        public List<WaybillLnquiry> GetWaybillLnquiry(string bh)
+        {
+            string sql = "select * from WaybillLnquiry";
+            return DBHelper.GetToList<WaybillLnquiry>(sql);
+        }
+    }
+}
---
+++ SwiftExpress/MODEL/Contraband/Contraband.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MODEL
+{
+    /// <summary>
+    /// 违禁品表
+    /// </summary>
+    public class Contraband
+    {
+        /// <summary>
+        /// 违禁品id
+        /// </summary>
+        public int ContrabandId { get; set; }
+        /// <summary>
+        /// 名称
+        /// </summary>
+        public string ItemName { get; set; }
+    }
+}
