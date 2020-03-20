5bfc09b04dc273a610d9e27b7e388c56751111d5
Zhou <379223538@qq.com>
2020-03-20 15:11:08 +0800

    Area控制器

---
+++ SwiftExpress/SwiftExpressApi/Controllers/Area/AreaController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using BLL.Area;
+using MODEL.Area;
+
+namespace SwiftExpressApi.Controllers.Area
+{
+    /// <summary>
+    /// 地区控制器
+    /// </summary>
+    public class AreaController : ApiController
+    {
+        #region  zrx 
+        AreaBLL areaBll = new AreaBLL();
+        /// <summary>
+        /// 获取省份
+        /// </summary>
+        /// <returns></returns>
+        public List<AreaModel> GetProvince()
+        {
+            return areaBll.GetProvince();
+        }
+        /// <summary>
+        /// 获取城市、区、县
+        /// </summary>
+        /// <param name="pid"></param>
+        /// <returns></returns>
+        public List<AreaModel> GetCity(int pid)
+        {
+            return areaBll.GetCity(pid);
+        }
+        #endregion
+    }
+}
