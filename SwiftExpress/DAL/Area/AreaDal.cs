994aeb5cd10ff6546df259faef7679c138210c4c
Zhou <379223538@qq.com>
2020-03-20 12:10:51 +0800

    添加Area部分代码

---
+++ SwiftExpress/DAL/Area/AreaDal.cs
@@ -0,0 +1,19 @@
+﻿using MODEL.Area;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Area
+{
+   public  class AreaDal
+    {
+        public List<AreaModel> GetAreas(int pid)
+        {
+            string sql = $"select  * from  Areatable where PId={pid}";
+            List<AreaModel> list = new List<AreaModel>();
+            return list;
+        }
+    }
+}
---
+++ SwiftExpress/MODEL/Area/AreaModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MODEL.Area
+{
+    /// <summary>
+    /// 地区表
+    /// </summary>
+   public class AreaModel
+    {
+        public int SId { get; set; }
+        public string SName { get; set; }
+        public int PId { get; set; }
+
+    }
+}
