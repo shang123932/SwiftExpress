0165726a9288200dd1a09abe8f05f79de8f4089d
尚星星 <1609659354>
2020-03-20 10:02:20 +0800

    api框架搭建完成

---
+++ SwiftExpress/ApiSDKClient/BaseResponse.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApiSDKClient
+{
+   public class BaseResponse
+    {
+        public bool IsRegistSuccess { get; set; }
+        /// <summary>
+        /// 状态为true表示接口请求成功
+        /// </summary>
+        public bool Status = true;
+        /// <summary>
+        /// 当状态失败时,提示消息
+        /// </summary>
+        public string Message { get; set; }
+    }
+}
