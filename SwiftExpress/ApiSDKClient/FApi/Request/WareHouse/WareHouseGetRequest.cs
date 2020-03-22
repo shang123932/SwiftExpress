74444c9dd68f1fc7665bc1c05dc9cf5dd70b5b54
Zhou <379223538@qq.com>
2020-03-22 22:28:09 +0800

    仓库及货物的部分代码

---
+++ SwiftExpress/ApiSDKClient/FApi/Request/WareHouse/WareHouseGetRequest.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApiSDKClient.FApi.Request.WareHouse
+{
+    public class WareHouseGetRequest : BaseRequest
+    {
+    }
+}
