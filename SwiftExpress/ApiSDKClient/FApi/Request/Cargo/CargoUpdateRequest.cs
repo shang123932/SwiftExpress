5e1b618b124b8ed12007ed2b09961b6af4701efe
Zhou <379223538@qq.com>
2020-03-23 23:30:49 +0800

    cargoBLL层的修改

---
+++ SwiftExpress/ApiSDKClient/FApi/Request/Cargo/CargoUpdateRequest.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApiSDKClient.FApi.Request.Cargo
+{
+  public class CargoUpdateRequest:BaseRequest
+    {
+
+    }
+}
