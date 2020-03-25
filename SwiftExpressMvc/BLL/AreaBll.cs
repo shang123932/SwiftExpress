1fc2a21b82106292993dd506f676a30afa283e45
Zhou <379223538@qq.com>
2020-03-25 23:51:42 +0800

    首页

--- SwiftExpressMvc/BLL/AreaBll.cs
+++ SwiftExpressMvc/BLL/AreaBll.cs
@@ -3,11 +3,34 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ApiSDKClient.FApi.Request;
+using ApiSDKClient.FApi.Request.Area;
+using ApiSDKClient.FApi.Response.Area;
 
 namespace BLL
 {
+    /// <summary>
+    /// 地区的BLL
+    /// </summary>
    public class AreaBll
     {
-
+        /// <summary>
+        /// 获取信息
+        /// </summary>
+        /// <param name="getRequest"></param>
+        /// <returns></returns>
+        public AreaProvinceResponse AreaGet(AreaProvinceRequest getRequest)
+        {
+            return ApiRequestHelper.Post<AreaProvinceRequest, AreaProvinceResponse>(getRequest);
+        }
+        /// <summary>
+        /// 获取信息
+        /// </summary>
+        /// <param name="getRequest"></param>
+        /// <returns></returns>
+        public AreaCityResponse AreaGet(AreaCityRequest getRequest)
+        {
+            return ApiRequestHelper.Post<AreaCityRequest, AreaCityResponse>(getRequest);
+        }
     }
 }
--- SwiftExpressMvc/BLL/WareHouseBll.cs
+++ SwiftExpressMvc/BLL/WareHouseBll.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using ApiSDKClient.FApi.Request.WareHouse;
+using ApiSDKClient.FApi.Response.WareHouse;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,8 +8,57 @@ using System.Threading.Tasks;
 
 namespace BLL
 {
+    /// <summary>
+    /// 仓库的BLL
+    /// </summary>
    public class WareHouseBll
     {
 
+        /// <summary>
+        /// 仓库添加
+        /// </summary>
+        /// <param name="addRequest"></param>
+        /// <returns></returns>
+        public WareHouseAddResponse WareHouseAdd(WareHouseAddRequest addRequest)
+        {
+            return ApiRequestHelper.Post<WareHouseAddRequest, WareHouseAddResponse>(addRequest);
+        }
+        /// <summary>
+        /// 仓库删除
+        /// </summary>
+        /// <param name="delRequest"></param>
+        /// <returns></returns>
+        public WareHouseDelResponse WareHouseDel(WareHouseDelRequest delRequest)
+        {
+            return ApiRequestHelper.Post<WareHouseDelRequest, WareHouseDelResponse>(delRequest);
+        }
+
+        /// <summary>
+        /// 获取一条信息
+        /// </summary>
+        /// <param name="getoneRequest"></param>
+        /// <returns></returns>
+        public WareHouseGetOneResponse WareHouseGetOne(WareHouseGetOneRequest getoneRequest)
+        {
+            return ApiRequestHelper.Post<WareHouseGetOneRequest, WareHouseGetOneResponse>(getoneRequest);
+        }
+        /// <summary>
+        /// 获取信息
+        /// </summary>
+        /// <param name="getRequest"></param>
+        /// <returns></returns>
+        public WareHouseGetResponse WareHouseGet(WareHouseGetRequest getRequest)
+        {
+            return ApiRequestHelper.Post<WareHouseGetRequest, WareHouseGetResponse>(getRequest);
+        }
+        /// <summary>
+        /// 保存仓库
+        /// </summary>
+        /// <param name="addRequest"></param>
+        /// <returns></returns>
+        public WareHouseUpdateResponse WareHouseSave(WareHouseAddRequest addRequest)
+        {
+            return ApiRequestHelper.Post<WareHouseAddRequest, WareHouseUpdateResponse>(addRequest);
+        }
     }
 }
