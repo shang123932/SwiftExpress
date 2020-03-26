fcdb34aaca51be60444c798a0b2a2d947675253f
尚星星 <1609659354>
2020-03-26 19:23:48 +0800

    staff

--- SwiftExpressMvc/SwiftExpressUI/Controllers/Contraband/ContrabandController.cs
+++ SwiftExpressMvc/SwiftExpressUI/Controllers/Contraband/ContrabandController.cs
@@ -6,7 +6,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 
-namespace SwiftExpressUI.Controllers.Contraband
+namespace SwiftExpressUI
 {
     public class ContrabandController : Controller
     {
