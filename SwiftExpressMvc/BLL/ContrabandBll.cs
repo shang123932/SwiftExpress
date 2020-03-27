8f5e6ea645c5d4c9db923f35f6f87130d03b359f
2417523 <62365872+2417523@users.noreply.github.com>
2020-03-27 21:27:57 +0800

    1

--- SwiftExpressMvc/BLL/ContrabandBll.cs
+++ SwiftExpressMvc/BLL/ContrabandBll.cs
@@ -1,5 +1,6 @@
 ﻿using ApiSDKClient;
-
+using ApiSDKClient.FApi.Request.Contraband;
+using ApiSDKClient.FApi.Response.Contraband;
 using System;
 using System.Collections.Generic;
 using System.Linq;
--- SwiftExpressMvc/SwiftExpressUI/Controllers/Shipp/ShippController.cs
+++ SwiftExpressMvc/SwiftExpressUI/Controllers/Shipp/ShippController.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-
+using BLL;
 namespace SwiftExpressUI
 {
     public class ShippController : Controller
@@ -12,6 +12,35 @@ namespace SwiftExpressUI
         public ActionResult Index()
         {
             return View();
+        }
+        ShippingInforBll bll = new ShippingInforBll();
+        /// <summary>
+        /// 添加库房
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult cargoAdd()
+        {
+            return View();
+        }
+        [HttpPost]
+        public JsonResult cargoAdd(ADDShippingInforRequest request)
+        {
+            return Json(bll(request));
+        }
+
+      
+        /// <summary>
+        /// 显示库房
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult cargoShow()
+        {
+            return View();
+        }
+        [HttpPost]
+        public JsonResult cargoShow(CargoGetRequest request)
+        {
+            return Json(cbll.CargoGet(request));
         }
     }
 }
