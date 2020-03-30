c267436559152d5c3b58a0c73ada04fa8c24b015
2417523 <62365872+2417523@users.noreply.github.com>
2020-03-30 20:08:41 +0800

    页面

--- SwiftExpressMvc/SwiftExpressUI/Controllers/Staff/StaffController.cs
+++ SwiftExpressMvc/SwiftExpressUI/Controllers/Staff/StaffController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using ApiSDKClient.FApi.Request.Staff;
+using BLL;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -12,6 +14,45 @@ namespace SwiftExpressUI
         public ActionResult Index()
         {
             return View();
+        }
+        StaffBll bll = new StaffBll();
+        /// <summary>
+        /// 添加库房
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult ADDStaff()
+        {
+            return View();
+        }
+        [HttpPost]
+        public JsonResult ADDStaff(ADDStaffRequest request)
+        {
+            return Json(bll.ADDStaff(request));
+        }
+
+
+        public ActionResult GetStaff()
+        {
+            return View();
+        }
+        [HttpPost]
+        public JsonResult GetStaff(GetStaffRequest request)
+        {
+            return Json(bll.GetStaff(request));
+        }
+        /// <summary>
+        /// 配送删除
+        /// </summary>
+        /// <returns></returns>
+        [HttpPost]
+        public JsonResult DeleteDistribution(DelStaffRequest request)
+        {
+            return Json(bll.DelStaff(request));
+        }
+        [HttpPost]
+        public JsonResult UpdateStaff(UpdateStaffRequest request)
+        {
+            return Json(bll.UpdateStaff(request));
         }
     }
 }
