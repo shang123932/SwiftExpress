3c773eb0de46187ef46bda3ab6a97e373752c8a4
2417523 <62365872+2417523@users.noreply.github.com>
2020-04-01 11:20:26 +0800

    1

--- SwiftExpressMvc/SwiftExpressUI/Controllers/Staff/StaffController.cs
+++ SwiftExpressMvc/SwiftExpressUI/Controllers/Staff/StaffController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using ApiSDKClient.FApi.Request.Staff;
+using BLL;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,11 +10,39 @@ namespace SwiftExpressUI
 {
     public class StaffController : Controller
     {
+        StaffBll bll = new StaffBll();
         // GET: Staff
         public ActionResult Index()
         {
             return View();
         }
-
+        public ActionResult GetStaff()
+        {
+            return View();
+        }
+        [HttpPost]
+        public JsonResult GetStaff(GetStaffRequest request)
+        {
+            return Json(bll.GetStaff(request));
+        }
+        //删除
+        public ActionResult DelStafft()
+        {
+            return View();
+        }
+        [HttpPost]
+        public JsonResult DelStaff(DelStaffRequest request)
+        {
+            return Json(bll.DelStaff(request));
+        }
+        public ActionResult UpdateStaff()
+        {
+            return View();
+        }
+        [HttpPost]
+        public JsonResult UpdateStaff(UpdateStaffRequest request)
+        {
+            return Json(bll.UpdateStaff(request));
+        }
     }
 }
